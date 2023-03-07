da21a98626aeaf1a0f3eb07b60965af7b14a5979
ovdekris <ovde.kris@gmail.com>
2023-03-07 10:05:34 +0100

    Styling our chart and doing some strunctures of data

--- dist/index.0641b553.js
+++ dist/index.0641b553.js
@@ -563,42 +563,26 @@ var _autoDefault = parcelHelpers.interopDefault(_auto);
 (async function() {
     const data = [
         {
-            year: 2010,
-            count: 10
+            x: "2016-12-25",
+            y: 20
         },
         {
-            year: 2011,
-            count: 20
-        },
-        {
-            year: 2012,
-            count: 15
-        },
-        {
-            year: 2013,
-            count: 25
-        },
-        {
-            year: 2014,
-            count: 22
-        },
-        {
-            year: 2015,
-            count: 30
-        },
-        {
-            year: 2016,
-            count: 28
+            x: "2016-12-26",
+            y: 10
         }
     ];
     new (0, _autoDefault.default)(document.getElementById("acquisitions"), {
         type: "bar",
+        options: {
+            indexAxis: "y",
+            min: "2020-09-02"
+        },
         data: {
-            labels: data.map((row)=>row.year),
+            labels: data.map((row)=>row.y),
             datasets: [
                 {
-                    label: "Acquisitions by year",
-                    data: data.map((row)=>row.count)
+                    label: "Acquisitions by day",
+                    data: data.map((row)=>row.x)
                 }
             ]
         }
--- src/app.js
+++ src/app.js
@@ -1,26 +1,32 @@
 import Chart from 'chart.js/auto'
 
 (async function() {
-    const data = [
-        { year: 2010, count: 10 },
-        { year: 2011, count: 20 },
-        { year: 2012, count: 15 },
-        { year: 2013, count: 25 },
-        { year: 2014, count: 22 },
-        { year: 2015, count: 30 },
-        { year: 2016, count: 28 },
-    ];
+    const data=
+        [{x: '2016-12-25', y: 20},
+            {x: '2016-12-26', y: 10}]
 
     new Chart(
         document.getElementById('acquisitions'),
         {
             type: 'bar',
+            options:{
+                indexAxis: 'y',
+                min:'2020-09-02',
+                // scales:{
+                //     x:{
+                //         type:"time",
+                //         time:{
+                //             unit:"day"
+                //         }
+                //     }
+                // }
+            },
             data: {
-                labels: data.map(row => row.year),
+                labels: data.map(row => row.y),
                 datasets: [
                     {
-                        label: 'Acquisitions by year',
-                        data: data.map(row => row.count)
+                        label: 'Acquisitions by day',
+                        data: data.map(row => row.x)
                     }
                 ]
             }
