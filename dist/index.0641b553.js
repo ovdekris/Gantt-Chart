1792369da1fb4fc2dd8ebfbf28589ed5e1b6dee5
ovdekris <ovde.kris@gmail.com>
2023-03-17 13:47:11 +0200

    Create a asigned Tasks and style our chart

--- dist/index.0641b553.js
+++ dist/index.0641b553.js
@@ -564,56 +564,74 @@ var _auto = require("chart.js/auto");
 var _autoDefault = parcelHelpers.interopDefault(_auto);
 (async function() {
     const ctx = document.getElementById("myChart");
-    const todayLine = {
-        id: "todayLine",
+    const assignedTasks = {
+        id: "assignedTasks",
         afterDatasetsDraw (chart, args, options) {
             const { ctx , data , chartArea: { top , bottom , left , right  } , scales: { x , y  }  } = chart;
-            ctx.save();
-            ctx.beginPath();
-            ctx.lineWidth = 3;
-            ctx.moveTo(x.getPixelForValue(new Date()), top);
-            ctx.lineTo(x.getPixelForValue(new Date()), bottom);
-            ctx.stroke();
+            ctx.font = "12px sans-serif bold";
+            ctx.fillStyle = "black";
+            ctx.fillText("text", 10, y.getPixelForValue(0));
         }
     };
+    console.log(assignedTasks);
     new (0, _autoDefault.default)(ctx, {
         type: "bar",
         data: {
             datasets: [
                 {
                     label: "Gant Chart",
-                    backgroundColor: [
-                        "#FFC0CB",
-                        "#fff",
-                        "#FFC0CB"
+                    borderSkipped: false,
+                    borderWidth: 2,
+                    borderColor: [
+                        "rgba(255, 10, 13,0.8)",
+                        "rgb(0,255,255)",
+                        "rgb(255,0,255)"
                     ],
+                    backgroundColor: [
+                        "rgba(255, 10, 13, 0.4)",
+                        "rgb(0,255,255,0.4)",
+                        "rgb(255,0,255,0.4)"
+                    ],
+                    borderRadius: 15,
+                    opacity: 90,
                     data: [
                         {
                             x: [
-                                "2023-01-09",
-                                "2023-01-01"
+                                "2023-03-09",
+                                "2023-03-01"
                             ],
-                            y: "Task 1"
+                            y: "Task 1",
+                            name: "Kris"
                         },
                         {
                             x: [
-                                "2023-02-08",
-                                "2023-02-03"
+                                "2023-03-08",
+                                "2023-03-03"
                             ],
-                            y: "Task 2"
+                            y: "Task 2",
+                            name: "John"
                         },
                         {
                             x: [
                                 "2023-03-07",
                                 "2023-03-02"
                             ],
-                            y: "Task 3"
+                            y: "Task 3",
+                            name: "Ariel"
                         }
                     ]
                 }
+            ],
+            plugins: [
+                assignedTasks
             ]
         },
         options: {
+            layout: {
+                padding: {
+                    left: 100
+                }
+            },
             indexAxis: "y",
             scales: {
                 x: {
@@ -621,8 +639,8 @@ var _autoDefault = parcelHelpers.interopDefault(_auto);
                     time: {
                         unit: "day"
                     },
-                    min: "2023-01-01",
-                    max: "2023-10-10"
+                    min: "2023-03-01",
+                    max: "2023-03-30"
                 }
             }
         }
--- src/app.js
+++ src/app.js
@@ -3,33 +3,42 @@ import 'chartjs-adapter-luxon';
 import Chart from 'chart.js/auto';
 (async function() {
     const ctx = document.getElementById('myChart');
-    const todayLine={
-        id: 'todayLine',
-        afterDatasetsDraw(chart, args, options){
-        const {ctx, data, chartArea:{top,bottom,left,right}, scales:{x,y}}=chart
-            ctx.save();
-            ctx.beginPath();
-            ctx.lineWidth =3;
-            ctx.moveTo(x.getPixelForValue(new Date()),top);
-            ctx.lineTo(x.getPixelForValue(new Date()),bottom);
-            ctx.stroke();
-
-         }
+    const assignedTasks={
+        id:'assignedTasks',
+        afterDatasetsDraw(chart, args, options) {
+            const {ctx, data, chartArea:{top,bottom,left,right}, scales:{x,y}}=chart;
+            ctx.font='12px sans-serif bold';
+            ctx.fillStyle='black';
+            ctx.fillText('text',10,y.getPixelForValue(0));
+        }
     }
+    console.log(assignedTasks);
     new Chart(ctx, {
+
         type: 'bar',
         data: {
             datasets: [{
                 label: 'Gant Chart',
-                backgroundColor: ['#FFC0CB','#fff','#FFC0CB'],
+                borderSkipped:false,
+                borderWidth: 2,
+                borderColor:["rgba(255, 10, 13,0.8)","rgb(0,255,255)","rgb(255,0,255)"],
+                backgroundColor: [ "rgba(255, 10, 13, 0.4)","rgb(0,255,255,0.4)","rgb(255,0,255,0.4)"],
+                borderRadius: 15,
+                opacity: 90,
                 data: [
-                    {x: ['2023-01-09','2023-01-01'], y: "Task 1"},
-                    {x: ['2023-02-08','2023-02-03'], y: "Task 2"},
-                    {x: ['2023-03-07','2023-03-02'], y: "Task 3"}],
+                    {x: ['2023-03-09','2023-03-01'], y: "Task 1", name:'Kris'},
+                    {x: ['2023-03-08','2023-03-03'], y: "Task 2", name: 'John'},
+                    {x: ['2023-03-07','2023-03-02'], y: "Task 3", name: 'Ariel'}],
 
             }],
+            plugins:[assignedTasks],
         },
         options: {
+            layout:{
+                padding:{
+                     left:100
+                }
+            },
             indexAxis: 'y',
             scales: {
                 x: {
@@ -37,8 +46,8 @@ import Chart from 'chart.js/auto';
                     time:{
                         unit: 'day'
                     },
-                    min:'2023-01-01',
-                    max: '2023-10-10'
+                    min:'2023-03-01',
+                    max: '2023-03-30'
                 }
             }
         }
