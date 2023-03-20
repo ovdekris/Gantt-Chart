719e3acb1ef973abfc551dacfbbb83388f466689
ovdekris <ovde.kris@gmail.com>
2023-03-20 22:25:53 +0200

    Create a structures for asignedTasks

--- src/app.js
+++ src/app.js
@@ -9,30 +9,34 @@ import Chart from 'chart.js/auto';
         afterDatasetsDraw(chart, args, options) {
             const {ctx, data, chartArea:{top,bottom,left,right}, scales:{x,y}}=chart;
             const icons=["\uf00c","\uf00d","\uf110"];
+            const angle=Math.PI/180;
             const colors=["rgba(255, 10, 13,0.8)","rgb(0,255,255)","rgb(255,0,255)"];
             ctx.save();
-            ctx.font='bold 12px FontAwesome';
-            // ctx.fillStyle=colors[1];
+            ctx.font='bold 10px FontAwesome';
             ctx.textBaseline='middle';
             data.datasets[0].data.map((data,index)=>{
-                ctx.arc(x,y,1,angleS,angleE,false);
+                ctx.beginPath();
+                ctx.fillStyle=colors[data.status];
+                ctx.arc(right+15,y.getPixelForValue(index),10,0,angle*360,false);
+                ctx.closePath();
+                ctx.fill();
+                ctx.fillStyle="white";
                 ctx.fillText(icons[data.status],right+10,y.getPixelForValue(index));
             })
             ctx.restore();
         }
     }
-    console.log('/uf00c')
     //assignedTasks plugin
     const assignedTasks={
         id:'assignedTasks',
         afterDatasetsDraw(chart, args, options) {
             const {ctx, data, chartArea:{top,bottom,left,right}, scales:{x,y}}=chart;
             ctx.save();
-            ctx.font='bold 12px FontAwesome';
+            ctx.font='bold 12px sans-serif';
             ctx.fillStyle='black';
             ctx.textBaseline='middle';
             data.datasets[0].data.map((data,index)=>{
-                ctx.fillText(data.name,10,y.getPixelForValue(index));
+                ctx.fillText(data.name,5,y.getPixelForValue(index));
                })
             ctx.restore();
 
